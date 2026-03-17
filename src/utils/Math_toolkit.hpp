94222e896a581ceccabf106692c38e6466872bc5
Tom <150679896+cqptomii@users.noreply.github.com>
2026-03-17 11:27:17 +0100

    Update Math toolkit to add fade curve function and linear interpolation function

--- src/utils/Math_toolkit.hpp
+++ src/utils/Math_toolkit.hpp
@@ -18,6 +18,15 @@ public:
     static double raised_inverted_cosi(double x);
     static double linear_identity(double x);
     static double curved_identity(double x);
+    static double dot(double x1, double y1, double z1, double x2, double y2, double z2){
+        return x1 * x2 + y1 * y2 + z1 * z2;
+    }
+    static double fade(double t){
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+    static double lerp(double a, double b, double t){
+        return a + t * (b - a);
+    }
 };
 
 
