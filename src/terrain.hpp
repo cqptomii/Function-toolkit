d463a6ca592b7e59b83b966048a140afebec8f95
Tom <150679896+cqptomii@users.noreply.github.com>
2026-03-18 11:09:25 +0100

    Update Landscape class to add functional warping

--- src/terrain.hpp
+++ src/terrain.hpp
@@ -37,8 +37,32 @@ class Landscape {
             freq *= lacunarity;
         }
 
-        return total / max_value;
+        return total / max_value; // Normalize the result to the range [-1, 1]
     }
+
+    double warping(double x, double y, double z){
+        double warp_amp = 0.5;
+        double warp_freq = 0.1;
+
+        double warp_x = fbm( x, y, z);
+        double warp_y = fbm( x + 5.2, y + 1.3, z + 2.7 );
+
+        return this->fbm(x + 4.0*warp_x, y + 4.0*warp_y, z);
+    }
+
+    double warping2(double x, double y, double z){
+        double warp_amp = 0.5;
+        double warp_freq = 0.1;
+
+        double q_warp_x = fbm( x, y, z);
+        double q_warp_y = fbm( x + 5.2, y + 1.3, z + 2.7 );
+
+        double r_warp_x = fbm( x + 4.0*q_warp_x + 1.7, y + 4.0*q_warp_y + 9.2, z);
+        double r_warp_y = fbm( x + 4.0*q_warp_x + 8.3, y + 4.0*q_warp_y + 2.8, z);
+        
+        return this->fbm(x + 4.0*r_warp_x, y + 4.0*r_warp_y, z);
+    }
+
 public:
     Landscape(NoiseType type, unsigned int seed){
         switch (type) {
@@ -82,33 +106,22 @@ public:
                 double x = i * scale;
                 double y = j * scale;
 
-                Vec2 q = {
-                    fbm(x * 0.001, y * 0.001, 0.0),
-                    fbm(x * 0.001 + 5.2, y * 0.001 + 1.3, 0.0)
-                };
-
                 // --- Continents ---
-                double continent = fbm(
-                    x * 0.002 + 4.0 * q.x,
-                    y * 0.002 + 4.0 * q.y,
-                    0.0
-                );
+                double continent = warping(x * 0.05, y * 0.05, 3.45);
                 continent = (continent + 1.0) * 0.5;
+                continent = pow(continent, 1.5);
 
                 // --- Mask terre ---
-                double mask = std::clamp((continent - 0.5) * 5.0, 0.0, 1.0);
+                double mask = Math_toolkit::smooth_step(0.3, 0.7, continent);
 
                 // --- Relief ---
-                double terrain = fbm(x * 0.02, y * 0.02, 0.0);
+                double terrain = fbm(x * 0.3, y * 0.3, 1.2456);
                 terrain = (terrain + 1.0) * 0.5;
 
                 // --- Montagnes ---
-                double mountains = fbm(x * 0.01, y * 0.01, 0.0);
-                mountains = pow(mountains, 3.0);
+                double height = terrain * mask;
 
-                double height = terrain + mountains;
-
-                map[i][j] = terrain * mask * max_height;
+                map[i][j] = height * max_height;
             }
         }
         return map;
